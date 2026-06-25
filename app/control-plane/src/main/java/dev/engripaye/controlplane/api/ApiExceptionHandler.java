7c78cfaf3df36e119b86e725afcd7b9f1ffda1dd
mac <@b.tunde.ipaye@gmail.com>
2026-06-25 16:25:11 +0100

    feat: updating control plane service by ENGR. IPAYE

--- app/control-plane/src/main/java/dev/engripaye/controlplane/api/ApiExceptionHandler.java
+++ app/control-plane/src/main/java/dev/engripaye/controlplane/api/ApiExceptionHandler.java
@@ -1,11 +1,23 @@
 package dev.engripaye.controlplane.api;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.ProblemDetail;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.net.URI;
 
 @RestControllerAdvice
 public class ApiExceptionHandler {
 
-    @ExceptionHandler
+    @ExceptionHandler(ResponseStatusException.class)
+    ProblemDetail status(ResponseStatusException exception, HttpServletRequest request){
+        var problem = ProblemDetail.forStatusAndDetail(exception.getStatusCode(), exception.getReason());
+        problem.setInstance(URI.create(request.getRequestURI()));
+
+
+    }
 }
 
