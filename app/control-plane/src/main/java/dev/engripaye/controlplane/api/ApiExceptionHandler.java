ab3b55195eb101212ba32469514dac1f00f84f11
mac <@b.tunde.ipaye@gmail.com>
2026-06-25 19:40:47 +0100

    feat: updated control plane service by ENGR. IPAYE

--- app/control-plane/src/main/java/dev/engripaye/controlplane/api/ApiExceptionHandler.java
+++ app/control-plane/src/main/java/dev/engripaye/controlplane/api/ApiExceptionHandler.java
@@ -1,7 +1,6 @@
 package dev.engripaye.controlplane.api;
 
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
