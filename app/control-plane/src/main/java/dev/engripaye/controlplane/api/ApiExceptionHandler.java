62f764411aeb40479e505e5a25999edc35121c84
mac <@b.tunde.ipaye@gmail.com>
2026-06-25 16:18:48 +0100

    feat: updating control plane service by ENGR. IPAYE

---
+++ app/control-plane/src/main/java/dev/engripaye/controlplane/api/ApiExceptionHandler.java
@@ -0,0 +1,11 @@
+package dev.engripaye.controlplane.api;
+
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ApiExceptionHandler {
+
+    @ExceptionHandler
+}
+
