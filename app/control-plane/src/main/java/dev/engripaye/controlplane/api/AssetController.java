c5533825e024dc03c003362a0c72eaa7a282975d
mac <@b.tunde.ipaye@gmail.com>
2026-06-25 19:43:45 +0100

    feat: updated control plane service by ENGR. IPAYE

---
+++ app/control-plane/src/main/java/dev/engripaye/controlplane/api/AssetController.java
@@ -0,0 +1,9 @@
+package dev.engripaye.controlplane.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class AssetController {
+}
