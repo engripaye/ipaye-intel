8757699fa189f45d3548515d4f9d1e632b669991
mac <@b.tunde.ipaye@gmail.com>
2026-06-26 11:24:14 +0100

    feat: updated control plane service by ENGR. IPAYE

--- app/control-plane/src/main/java/dev/engripaye/controlplane/api/AssetController.java
+++ app/control-plane/src/main/java/dev/engripaye/controlplane/api/AssetController.java
@@ -1,11 +1,22 @@
 package dev.engripaye.controlplane.api;
 
+import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import tools.jackson.databind.ObjectMapper;
 
 @RestController
-@RequestMapping
+@RequestMapping("/api/v1/projects/{projectId}/assets")
 public class AssetController {
+
+    private final JdbcClient jdbcClient;
+    private final ObjectMapper objectMapper;
+
+    public AssetController(JdbcClient jdbcClient, ObjectMapper objectMapper){
+        this.jdbcClient = jdbcClient;
+        this.objectMapper = objectMapper;
+    }
+
 
     
 }
