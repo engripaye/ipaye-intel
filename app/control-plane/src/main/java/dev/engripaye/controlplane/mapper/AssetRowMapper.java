f900e311cc64ac6fe3bf6594d5447c7470ade7fd
mac <@b.tunde.ipaye@gmail.com>
2026-06-26 12:14:05 +0100

    feat: updated control plane service by ENGR. IPAYE

---
+++ app/control-plane/src/main/java/dev/engripaye/controlplane/mapper/AssetRowMapper.java
@@ -0,0 +1,39 @@
+package dev.engripaye.controlplane.mapper;
+
+import dev.engripaye.controlplane.dto.AssetResponse;
+import dev.engripaye.controlplane.model.AssetType;
+import org.springframework.stereotype.Component;
+import tools.jackson.databind.ObjectMapper;
+
+import javax.swing.tree.RowMapper;
+import javax.swing.tree.TreePath;
+import java.sql.ResultSet;
+import java.util.UUID;
+
+@Component
+public class AssetRowMapper implements RowMapper<AssetResponse> {
+
+    private final ObjectMapper objectMapper;
+
+
+    public AssetRowMapper(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
+
+
+    @Override
+    public AssetResponse mapRow(ResultSet rs, int rowNum) throws Exception{
+
+        return new AssetResponse(
+                rs.getObject("id", UUID.class),
+                rs.getObject("organization_id", UUID.class),
+                rs.getObject("project_id", UUID.class),
+                rs.getString("name"),
+                AssetType.valueOf(rs.getString("type")),
+                parse(rs.getString("content")),
+                rs.getTimestamp("created_At").toInstant(),
+
+        )
+
+    }
+}
