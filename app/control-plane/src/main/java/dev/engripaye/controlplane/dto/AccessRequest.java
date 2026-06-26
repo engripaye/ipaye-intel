2c8bb0ae811124edcda2b0b2974974773a6158f4
mac <@b.tunde.ipaye@gmail.com>
2026-06-26 11:53:14 +0100

    feat: updated control plane service by ENGR. IPAYE

--- app/control-plane/src/main/java/dev/engripaye/controlplane/dto/AccessRequest.java
+++ app/control-plane/src/main/java/dev/engripaye/controlplane/dto/AccessRequest.java
@@ -2,6 +2,9 @@ package dev.engripaye.controlplane.dto;
 
 import dev.engripaye.controlplane.model.AssetType;
 import com.fasterxml.jackson.databind.JsonNode;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 public record AccessRequest (
         @NotBlank
