b77094e761338a6103774e555bc8045664c2e4c2
mac <@b.tunde.ipaye@gmail.com>
2026-06-26 12:29:16 +0100

    feat: updated control plane service by ENGR. IPAYE

--- app/control-plane/src/main/java/dev/engripaye/controlplane/repository/AssetRepository.java
+++ app/control-plane/src/main/java/dev/engripaye/controlplane/repository/AssetRepository.java
@@ -1,9 +1,29 @@
 package dev.engripaye.controlplane.repository;
 
+import dev.engripaye.controlplane.dto.AssetResponse;
+import dev.engripaye.controlplane.mapper.AssetRowMapper;
+import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
 
 @Repository
 public class AssetRepository {
 
-    private
+    private final JdbcClient jdbcClient;
+    private final AssetRowMapper mapper;
+
+
+    public AssetRepository(JdbcClient jdbcClient, AssetRowMapper mapper) {
+        this.jdbcClient = jdbcClient;
+        this.mapper = mapper;
+    }
+
+    public AssetResponse create(
+            UUID organizationId,
+            UUID projectId,
+            CreateAssetRequest request
+    ){
+
+    }
 }
